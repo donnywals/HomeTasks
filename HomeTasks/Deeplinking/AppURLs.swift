e1127804da725f6e90d518cddf3f37c47cb22b28
Donny Wals <donnywals@gmail.com>
2020-07-05 16:28:49 +0200

    updated links

--- HomeTasks/Deeplinking/AppURLs.swift
+++ HomeTasks/Deeplinking/AppURLs.swift
@@ -42,3 +42,38 @@ extension URL {
     case task(id: UUID)
   }
 }
+
+enum TabIdentifier: Hashable {
+  case home, settings
+}
+
+enum PageIdentifier: Hashable {
+  case todoItem(id: UUID)
+}
+
+extension URL {
+  var detailPage: PageIdentifier? {
+    guard let tabIdentifier = tabIdentifier,
+          pathComponents.count > 1,
+          let uuid = UUID(uuidString: pathComponents[1]) else {
+      return nil
+    }
+    
+    switch tabIdentifier {
+    case .home: return .todoItem(id: uuid)
+    default: return nil
+    }
+  }
+  
+  var tabIdentifier: TabIdentifier? {
+    guard isDeeplink else {
+      return nil
+    }
+
+    switch host {
+    case "home": return .home // matches my-url-scheme://home/
+    case "settings": return .settings // matches my-url-scheme://settings/
+    default: return nil
+    }
+  }
+}
